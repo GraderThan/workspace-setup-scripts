deafe4ac3b63dec4a3a8b2a55d6e979cdaa674f4
H Williams <60046112+begraderthan@users.noreply.github.com>
2025-10-05 17:03:22 -0400

    Update c++ setup.sh to work with xeus-cling installed in nix store

--- cpp/setup.sh
+++ cpp/setup.sh
@@ -13,30 +13,8 @@ PROJECT_DIR=${1:-$DEFAULT_PROJECT_DIR}
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
 setupJupyterKernels(){
-  # Check if the environment exists
-  if ! micromamba env list | grep -q '^xeus-cling\s'; then
-    echo "Environment 'xeus-cling' not found. Creating it..."
-    micromamba create -n xeus-cling --yes
-  else
-    echo "Environment 'xeus-cling' already exists."
-  fi
-  
-  # Check if packages are installed in the environment
-  if ! micromamba list -n xeus-cling | grep -q '^xeus-cling'; then
-    echo "Installing xeus-cling and xwidgets..."
-    micromamba install xeus-cling xwidgets -c conda-forge -n xeus-cling --yes
-  else
-    echo "Packages xeus-cling and xwidgets already installed."
-  fi
-  
-  # Check if the Jupyter kernel is already installed
-  KERNEL_DIR="$HOME/.local/share/jupyter/kernels/xcpp17"
-  if [ ! -d "$KERNEL_DIR" ]; then
-    echo "Installing Jupyter kernel for xeus-cling..."
-    jupyter kernelspec install /home/developer/micromamba/envs/xeus-cling/share/jupyter/kernels/xcpp17 --user
-  else
-    echo "Jupyter kernel already installed."
-  fi
+  KPATH=$(echo /nix/store/*-xeus-cling-*/share/jupyter/kernels/xcpp17)
+  jupyter kernelspec install "$KPATH" --user
 }
 
 (
